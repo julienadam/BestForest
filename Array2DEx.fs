ed17a73237e4a90dba06b30fd17e3f778046e84a
Julien Adam <julien.adam@gmail.com>
2023-07-01 23:38:37 +0200

    Extracted array2D stuff and started profiling
    Next step is to optimize the lookups :
    - Change the tree definition from string to int
    - Change the interaction mapping from hashmap to 2D matrix using the int
      tree type

---
+++ Array2DEx.fs
@@ -0,0 +1,29 @@
+﻿namespace BestForest
+
+[<AutoOpen>]
+module Array2DEx = 
+
+    let getAdjacent row col (grid:'T[,]) = seq {
+        if row > 0 then
+            yield ((row - 1), col, grid.[(row - 1), col])
+        if row < ((grid |> Array2D.length1) - 1) then
+            yield ((row + 1), col, grid.[(row + 1), col])
+        if col > 0 then
+            yield (row, (col - 1), grid.[row, (col - 1)])
+        if col < ((grid |> Array2D.length2) - 1) then
+            yield (row, (col + 1), grid.[row, (col + 1)])
+        if row > 0 && col > 0 then
+            yield ((row - 1), (col - 1), grid.[(row - 1), (col - 1)])
+        if row > 0 && col < ((grid |> Array2D.length2) - 1) then
+            yield ((row - 1), (col + 1), grid.[(row - 1), (col + 1)])
+        if row <((grid |> Array2D.length1) - 1) && col > 0 then
+            yield ((row + 1), (col - 1), grid.[(row + 1), (col - 1)])
+        if row <((grid |> Array2D.length1) - 1) && col < ((grid |> Array2D.length2) - 1) then
+            yield ((row + 1), (col + 1), grid.[(row + 1), (col + 1)])
+    }
+
+    let enumArray2d (array:'a[,]) = seq {
+        for i = 0 to (array |> Array2D.length1) - 1 do
+            for j = 0 to (array |> Array2D.length2) - 1 do
+                yield i,j, array.[i,j]
+    }
--- Support.fs
+++ Support.fs
@@ -43,6 +43,7 @@ module Support =
         Step0_1 : StepLimits
         Step1_2 : StepLimits
         Step2_3 : StepLimits
+        // TODO change that to a 2D matrix of scores
         Interactions : Map<string, float>
     }
     with 
@@ -108,31 +109,6 @@ module Support =
                 |> Seq.map fst
                 |> Seq.toList
         )
-
-    let getAdjacent row col (grid:'T[,]) = seq {
-        if row > 0 then
-            yield ((row - 1), col, grid.[(row - 1), col])
-        if row < ((grid |> Array2D.length1) - 1) then
-            yield ((row + 1), col, grid.[(row + 1), col])
-        if col > 0 then
-            yield (row, (col - 1), grid.[row, (col - 1)])
-        if col < ((grid |> Array2D.length2) - 1) then
-            yield (row, (col + 1), grid.[row, (col + 1)])
-        if row > 0 && col > 0 then
-            yield ((row - 1), (col - 1), grid.[(row - 1), (col - 1)])
-        if row > 0 && col < ((grid |> Array2D.length2) - 1) then
-            yield ((row - 1), (col + 1), grid.[(row - 1), (col + 1)])
-        if row <((grid |> Array2D.length1) - 1) && col > 0 then
-            yield ((row + 1), (col - 1), grid.[(row + 1), (col - 1)])
-        if row <((grid |> Array2D.length1) - 1) && col < ((grid |> Array2D.length2) - 1) then
-            yield ((row + 1), (col + 1), grid.[(row + 1), (col + 1)])
-    }
-
-    let enumArray2d (array:'a[,]) = seq {
-        for i = 0 to (array |> Array2D.length1) - 1 do
-            for j = 0 to (array |> Array2D.length2) - 1 do
-                yield i,j, array.[i,j]
-    }
 
     let getScoreForCell trees i j treeType =
         if treeType = "NA" then 
